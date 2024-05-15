af20580d648bdf06572c32c271948a559bce7701
Ashley <kolk@MacBook-Air-van-Ashley.local>
2024-05-15 14:21:32 +0200

    v 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [state, setState] = useState(1);
+
+
+
+  function GetRadio(radio)
+  {
+    setState(radio.target.value);
+
+
+
+ }
+
+  function GetRating()
+  {
+    
+    let rating = state;
+    console.log(rating);
+    alert(`you ave us a rating of ${rating}` );
+
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='background'>
+        <header className="experince-rating">
+          <h1 className='heading'>Tell us about your experince</h1>
+
+          <p className='paragraph'>We value your input! Please tell us how your experince was</p>
+
+          <ul id='rating'>
+
+             
+            <li>
+              <input type='radio' name='rating' value={1} onChange={GetRadio} id="1" />
+              <label for='1' className='rating-label'>1</label>
+              
+            </li>
+
+            <li>
+              <input type='radio' name='rating' value={2} onChange={GetRadio} id="2" />
+              <label for='2' className='rating-label'>2</label>
+            </li>
+            <li>
+              <input type='radio' name='rating' value={3} onChange={GetRadio} id="3" />
+              <label for='3' className='rating-label'>3</label> 
+            </li>
+
+            <li>
+              <input type='radio' name='rating' value={4} onChange={GetRadio} id="4" />
+              <label for='4' className='rating-label'>4</label>
+            </li>
+            
+            <li>
+              <input type='radio' name='rating' value={5} onChange={GetRadio} id="5" />
+              <label for='5' className='rating-label'>5</label>
+              
+            </li>
+
+          </ul>
+
+          <button className='submit-button' type='submit' onClick={GetRating}>submit</button>
+        </header>
+      </div>
     </div>
   );
+  
 }
+
+
 
 export default App;
