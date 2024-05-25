cafc281e4a7d9430497407046d748d4ac4fd7ed4
theHTMLSpot <121297730+theHTMLSpot@users.noreply.github.com>
2024-05-25 10:44:18 +0000

    h

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useEffect, useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes,
-  useLocation
-} from "react-router-dom";
+import './index.css'
+import { useState } from 'react';
+import {React} from 'react'
 
 
 function App()
 {
-  return(
-  <Router>
-    <Routes>
-        <Route exact path='/' element={<Rating />}/>
-        <Route path='/thanks' element={<Thanks />}/>
-    </Routes>
-  </Router>
-  );
-}
-
-function Rating(props) {
-
-  const [rating, setRating] = useState(1);
-
-
+  const [rating, setRating] = useState();
 
   function GetRadio(radio)
   {
     setRating(radio.target.value);
   }
 
-
- 
   return (
     <div className="App">
 
@@ -74,69 +53,13 @@ function Rating(props) {
             </li>
 
           </ul>
-          <Link to='/thanks' state={{rating}}>
             <button className='submit-button' type='submit' >
               submit
             </button>
-          </Link>
         </header>
       </div>
     </div>
   );
-  
-}
-function Thanks()
-{
-
-  const [averageRating, setResponse] = useState();
-
-  useEffect( () => {
-    const asyncFn = async ()=>{
-      const responseHttp = await fetch("http://localhost:8000/average-rating");
-      const response =  await responseHttp.text();
-      console.log(response);
-      setResponse(response);
-    }
-    asyncFn();
-   
-    
-  }, [])
-  
-
-  let response = "";
-
-  const { state } = useLocation();
-
-  
-  let rating = state.rating;
-
-  if (rating<3)
-    {
-      response = `you gave us a ${rating} Awh thats too bad what can we do better`;
-    }
-  else if (rating >=3 && rating<5)
-    {
-      response = `you gave us a ${rating} Okay thats nice`;
-    }
-    else
-    {
-      response = `you gave us a ${rating} Thanks for the perfect rating`;
-    }
-  return(
-      <div className="App">
-  
-        <div className='background'>
-          <header className="experince-rating">
-            <h1 className='heading'>Thanks for telling us about your experince</h1>
-            
-            <p className='paragraph'>{response}</p>
-  
-            <p className='paragraph'>{averageRating}</p>
-          </header>
-        </div>
-      </div>
-  );
 }
 
-
-export default App;
+export default App();
